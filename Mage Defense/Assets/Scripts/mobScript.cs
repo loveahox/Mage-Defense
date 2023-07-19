410b193d07d77de174f66b2478407e66c6c7ba3d
iffymist <jaydenye22@gmail.com>
2023-07-19 09:54:47 -0400

    update
    made map and added entities

---
+++ Mage Defense/Assets/Scripts/mobScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class mobScript : MonoBehaviour
+{
+    public float health = 10f;
+    public float damage = 25;
+    public NavMeshAgent agent;
+    public float damageSpeed=.5f;
+    private Vector3 endLocation= new Vector3(-264f, -23.4f, -.9f);
+    bool isAttacking = false;
+    goblinAttack gAttack;
+    porkyAttack pAttack;
+    spiderAttack sAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gAttack = GetComponent<goblinAttack>();
+        agent.SetDestination(endLocation);
+    }
+    public void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    IEnumerator wait()
+    {
+        isAttacking = true;
+        yield return new WaitForSeconds(damageSpeed);
+        isAttacking = false;
+    }
+    public void attack()
+    {
+        if (!isAttacking)
+        {
+            if(gameObject.name.Equals("Goblin"))
+            {
+                gAttack.playAnim();
+            }
+            if (gameObject.name.Equals("Spider"))
+            {
+                sAttack.playAnim();
+            }
+            if (gameObject.name.Equals("Porky"))
+            {
+                pAttack.playAnim();
+            }
+            GameManager.gm.attacking(damage);
+            StartCoroutine(wait());
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance<1)
+        {
+            attack();
+        }
+    }
+}
