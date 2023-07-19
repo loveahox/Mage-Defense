75419b55d26586f6d6d8725fac4dc4226f2b2897
Mere <meri.oest@gmail.com>
2023-07-19 10:36:09 -0400

    Update StartMenu.cs

--- Mage Defense/Assets/Scripts/StartMenu.cs
+++ Mage Defense/Assets/Scripts/StartMenu.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
+public float PlayButton;
+private Button button;
 {
    public string newGameLevel = "Level1";
    public void NewGameButton()
    {
     SceneManager.LoadScene(newGameLevel);
    }
+// Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(PlayButton);
+    }
 }
