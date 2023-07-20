5789fe8c1c64b56095eb7f33245fb48da63f44d4
Big-Guy-25 <cooper01px2025@saschina.org>
2023-07-20 13:10:28 -0400

    bababooey

--- Mage Defense/Assets/CooperStuff/Scripts/GlueGunnerScript.cs
+++ Mage Defense/Assets/CooperStuff/Scripts/GlueGunnerScript.cs
@@ -13,12 +13,10 @@ public class GlueGunnerScript : MonoBehaviour
     public GameObject targetObj;
     public Transform target;
 
-<<<<<<< Updated upstream
+
     public bool firing = false;
     public bool alreadyTargeting = false;
-=======
-    
->>>>>>> Stashed changes
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +31,12 @@ public class GlueGunnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
 
         target = targetObj.transform;
-=======
+
         
->>>>>>> Stashed changes
+
         transform.LookAt(target);
 
         
