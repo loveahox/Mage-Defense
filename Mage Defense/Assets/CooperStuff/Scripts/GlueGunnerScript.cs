55c3a938c87d93762e5bcbd751ba6633087695de
Gwello06 <milorios05@gmail.com>
2023-07-20 13:19:32 -0400

    NEWEST BESTEST WORKING VERSION
    pls work.

--- Mage Defense/Assets/CooperStuff/Scripts/GlueGunnerScript.cs
+++ Mage Defense/Assets/CooperStuff/Scripts/GlueGunnerScript.cs
@@ -13,12 +13,9 @@ public class GlueGunnerScript : MonoBehaviour
     public GameObject targetObj;
     public Transform target;
 
-<<<<<<< Updated upstream
     public bool firing = false;
     public bool alreadyTargeting = false;
-=======
     
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +30,9 @@ public class GlueGunnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
 
         target = targetObj.transform;
-=======
         
->>>>>>> Stashed changes
         transform.LookAt(target);
 
         
